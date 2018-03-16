03df380a5a0b1375bd7d45f0b3cba729d8b1084e
Nhu Nguyen <nhu313@gmail.com>
2018-03-16 09:19:17 -0400

    Added mean test

--- src/test/java/ArrayUtilitiesTest.java
+++ src/test/java/ArrayUtilitiesTest.java
@@ -20,4 +20,12 @@ public class ArrayUtilitiesTest {
 //        int[] actual = ArrayUtilities.removeDuplicate(values);
 //        Assert.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void testMean(){
+        int[] values = {3, 5, 8, 1};
+        double expected = 8.5;
+//        double actual = ArrayUtilities.mean(values);
+        Assert.assertEquals(1, 1.5, 0.10);
+    }
 }
