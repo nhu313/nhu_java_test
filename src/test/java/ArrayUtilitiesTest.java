e6d23e4f3450b6a19db958855e7209f4c28be6e2
Nhu Nguyen <nhu313@gmail.com>
2018-03-16 09:39:24 -0400

    updated test

--- src/test/java/ArrayUtilitiesTest.java
+++ src/test/java/ArrayUtilitiesTest.java
@@ -6,6 +6,22 @@ import java.util.List;
 public class ArrayUtilitiesTest {
 
     @Test
+    public void testMoveLastToFirstWithOneItem(){
+        int[] values = {4};
+        int[] expected = {4};
+//        ArrayUtilities.moveLastToFirst(values);
+//        Assert.assertArrayEquals(expected, values);
+    }
+
+    @Test
+    public void testMoveLastToFirstWithTwoItems(){
+        int[] values = {13, 8};
+        int[] expected = {8, 13};
+//        ArrayUtilities.moveLastToFirst(values);
+//        Assert.assertArrayEquals(expected, values);
+    }
+
+    @Test
     public void testMoveLastToFirst(){
         int[] values = {4, 6, 2, 9};
         int[] expected = {9, 4, 6, 2};
@@ -14,11 +30,43 @@ public class ArrayUtilitiesTest {
     }
 
     @Test
+    public void testRemoveDuplicateWithOnlyOneItem(){
+        int[] values = {12};
+        int[] expected = {12};
+//        int[] actual = ArrayUtilities.removeDuplicate(values);
+//        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveDuplicateWithSameValue(){
+        int[] values = {5, 5};
+        int[] expected = {5};
+//        int[] actual = ArrayUtilities.removeDuplicate(values);
+//        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void testRemoveDuplicate(){
         int[] values = {4, 4, 6, 2, 9, 9, 9, 9};
         int[] expected = {4, 6, 2, 9};
 //        int[] actual = ArrayUtilities.removeDuplicate(values);
 //        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testMeanWithOneItem(){
+        int[] values = {5};
+        double expected = 5.0;
+//        double actual = ArrayUtilities.mean(values);
+//        Assert.assertEquals(expected, actual, 0.10);
+    }
+
+    @Test
+    public void testMeanWithTwoItem(){
+        int[] values = {2, 4};
+        double expected = 3.0;
+//        double actual = ArrayUtilities.mean(values);
+//        Assert.assertEquals(expected, actual, 0.10);
     }
 
     @Test
